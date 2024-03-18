02dd87b8f485f9dc2894d4610251a1cb6a0dda53
Abideen Shittu <110542235+jrshittu@users.noreply.github.com>
2024-03-18 18:10:21 +0100

    Update chatbot_wizard.py

--- chatbot_wizard.py
+++ chatbot_wizard.py
@@ -7,7 +7,7 @@ from taipy.gui import Gui, notify
 # load Mistral-7B-Instruct-v0.1-GGUF, Set gpu_layers to the number of layers to offload to GPU. The value is set to 0 because no GPU acceleration is available on my current system.
 llm = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-Instruct-v0.1-GGUF", model_file="mistral-7b-instruct-v0.1.Q4_K_M.gguf", model_type="mistral", gpu_layers=0)
 
-# call the model to generate text, starting with the prompt "AI is going to"
+# nitialize the `prompt` and `response` variables as empty strings.
 prompt = ""
 response = ""
 
